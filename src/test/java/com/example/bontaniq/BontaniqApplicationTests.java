e92d7234cf2a5406de3ea3f76d51831b754c1e18
Gabrielle Ynara <gabrielleynara@ymail.com>
2023-10-19 10:44:53 -0500

    Add dependencies

---
+++ src/test/java/com/example/bontaniq/BontaniqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bontaniq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BontaniqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
