a463f357319ce13c822fa391e29decce62d8cf04
Gabrielle Ynara <gabrielleynara@ymail.com>
2023-10-19 12:01:39 -0500

    Implement user_registration.feature

---
+++ src/test/java/definitions/UserRegistrationDefs.java
@@ -0,0 +1,10 @@
+package definitions;
+
+import io.cucumber.java.en.Given;
+
+public class UserRegistrationDefs {
+    @Given("I am on the registration page")
+    public void iAmOnTheRegistrationPage() {
+
+    }
+}
