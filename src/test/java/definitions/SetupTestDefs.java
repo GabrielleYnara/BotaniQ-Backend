1bdf1ecc28121a01858f005c8acdd868f1d04507
Gabrielle Ynara <gabrielleynara@ymail.com>
2023-10-19 19:21:54 -0500

    Add a method for generating http header

--- src/test/java/definitions/SetupTestDefs.java
+++ src/test/java/definitions/SetupTestDefs.java
@@ -4,6 +4,7 @@ import com.example.bontaniq.BontaniqApplication;
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpHeaders;
 
 @CucumberContextConfiguration
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = BontaniqApplication.class)
@@ -12,4 +13,11 @@ public class SetupTestDefs {
 
     @LocalServerPort
     protected String port;
+
+    protected HttpHeaders createAuthenticatedHeader(String token) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json");
+        headers.add("Authorization", "Bearer " + token);
+        return headers;
+    }
 }
