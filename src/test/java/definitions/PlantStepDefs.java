5d06e431d749c8bd86978a1d30812a4e66597254
Gabrielle Ynara <gabrielleynara@ymail.com>
2023-10-21 18:47:54 -0500

    Implement View Singular Plant test scenarios

--- src/test/java/definitions/PlantStepDefs.java
+++ src/test/java/definitions/PlantStepDefs.java
@@ -1,10 +1,10 @@
 package definitions;
 
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.RestAssured;
 import io.restassured.specification.RequestSpecification;
-import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.Assert;
 import org.springframework.http.HttpStatus;
@@ -15,6 +15,7 @@ public class PlantStepDefs extends SetupTestDefs{
     private static final Logger log = Logger.getLogger(PlantStepDefs.class.getName());
     private String name;
     private String type;
+    private int plantId;
     @When("I create a plant name {string} and type {string}")
     public void iCreateAPlantNameAndType(String name, String type) {
         log.info("I create a plant name " + name + " and type " + type);
@@ -53,5 +54,35 @@ public class PlantStepDefs extends SetupTestDefs{
         } catch (Exception e) {
             log.severe("Something went wrong during plant creation with missing info. \nError: " + e.getMessage());
         }
+    }
+
+    @Given("I provide a valid plant")
+    public void iProvideAValidPlant() {
+        log.info("I provide a valid plant");
+        this.plantId = 1;
+    }
+
+    @When("I request to view a singular plant in the garden")
+    public void iRequestToViewASingularPlantInTheGarden() {
+        log.info("I request to view a singular plant in the garden");
+        try{
+            RestAssured.baseURI = BASE_URL;
+            RequestSpecification request = RestAssured.given();
+            request.headers(createAuthenticatedHeader(token));
+            response = request.get(BASE_URL + port + "/gardens/1/plants/" + plantId + "/");
+        }catch (Exception e){
+            log.severe("Something went wrong while requesting to view a plant.");
+        }
+    }
+
+    @Then("I should see the plants details")
+    public void iShouldSeeThePlantsDetails() {
+        Assert.assertEquals(HttpStatus.OK, response.getStatusCode());
+        log.info("I should see the plants details");
+    }
+
+    @Given("I provide an invalid plant")
+    public void iProvideAnInvalidPlant() {
+        this.plantId = -1;
     }
 }
