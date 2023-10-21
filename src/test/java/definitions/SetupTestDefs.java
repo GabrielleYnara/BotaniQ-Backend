ff0e4a55d61f083ec1eeadacb47c7f8a95edc1a5
Gabrielle Ynara <gabrielleynara@ymail.com>
2023-10-20 21:55:27 -0500

    Update, add Response variable to SetupTestDefs to be shared across testing cases

--- src/test/java/definitions/SetupTestDefs.java
+++ src/test/java/definitions/SetupTestDefs.java
@@ -2,6 +2,7 @@ package definitions;
 
 import com.example.bontaniq.BontaniqApplication;
 import io.cucumber.spring.CucumberContextConfiguration;
+import io.restassured.response.Response;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpHeaders;
@@ -14,6 +15,7 @@ public class SetupTestDefs {
     @LocalServerPort
     protected String port;
     protected String token;
+    protected static Response response;
     protected HttpHeaders createAuthenticatedHeader(String token) {
         HttpHeaders headers = new HttpHeaders();
         headers.add("Content-Type", "application/json");
