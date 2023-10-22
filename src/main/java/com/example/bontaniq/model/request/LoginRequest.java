9d1c695cf6f955a4aecdf05673854a64ad3a621d
Gabrielle Ynara <gabrielleynara@ymail.com>
2023-10-22 11:49:20 -0500

    Implement LoginRequest model

---
+++ src/main/java/com/example/bontaniq/model/request/LoginRequest.java
@@ -0,0 +1,15 @@
+package com.example.bontaniq.model.request;
+
+public class LoginRequest {
+
+    private String emailAddress;
+    private String password;
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
