73e809bdc5e7a391144476161f3ad31aa910be88
Gabrielle Ynara <gabrielleynara@ymail.com>
2023-10-22 11:50:55 -0500

    Implement LoginResponse model

---
+++ src/main/java/com/example/bontaniq/model/response/LoginResponse.java
@@ -0,0 +1,11 @@
+package com.example.bontaniq.model.response;
+
+public class LoginResponse {
+
+    public String jwt;
+
+    public LoginResponse(String jwt) {
+        this.jwt = jwt;
+    }
+
+}
