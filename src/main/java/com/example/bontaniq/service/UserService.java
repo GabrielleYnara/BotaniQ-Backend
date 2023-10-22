ee04cd0492cfd52c76a6cd38565e00b8369cd1c4
Gabrielle Ynara <gabrielleynara@ymail.com>
2023-10-22 13:09:47 -0500

    Update UserService, add relevant JavaDoc strings

--- src/main/java/com/example/bontaniq/service/UserService.java
+++ src/main/java/com/example/bontaniq/service/UserService.java
@@ -5,15 +5,32 @@ import com.example.bontaniq.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+/**
+ * Represents the User Service, responsible for housing business logic related to users.<br>
+ *
+ * This class serves as an intermediary between the controller and the repository,
+ * invoking the repository to perform CRUD operations on users.
+ * */
 @Service
 public class UserService {
     private final UserRepository userRepository;
 
+    /**
+     * Injects dependencies and enables userService to access the resources.
+     * @param userRepository
+     */
     @Autowired
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
+    /**
+     * Finds a User entity based on the provided email address.
+     *
+     * @param emailAddress The email address to search for.
+     * @return The User entity,
+     *         or null if no matching user is found.
+     */
     public User findUserByEmailAddress(String emailAddress){
         return userRepository.findUserByEmailAddress(emailAddress);
     }
