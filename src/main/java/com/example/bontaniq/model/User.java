8115c370fd710278c382743775e7fa185bb5dbaa
Gabrielle Ynara <gabrielleynara@ymail.com>
2023-10-22 11:58:54 -0500

    Implement User model

---
+++ src/main/java/com/example/bontaniq/model/User.java
@@ -0,0 +1,46 @@
+package com.example.bontaniq.model;
+
+public class User {
+    private Long id;
+    private String emailAddress;
+    private String password;
+
+    public User() {
+    }
+
+    public User(Long id, String emailAddress, String password) {
+        this.id = id;
+        this.emailAddress = emailAddress;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", emailAddress='" + emailAddress + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
