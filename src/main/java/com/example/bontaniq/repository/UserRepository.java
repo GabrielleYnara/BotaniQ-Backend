2599d101d8dbb877b63c9f65949fed6dc82b7936
Gabrielle Ynara <gabrielleynara@ymail.com>
2023-10-22 12:58:32 -0500

    Implement UserRepository with custom findUserByEmailAddress method

--- src/main/java/com/example/bontaniq/repository/UserRepository.java
+++ src/main/java/com/example/bontaniq/repository/UserRepository.java
@@ -6,4 +6,11 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    /**
+     * Checks the database if a user with given emailAddress exists.
+     * @param emailAddress
+     * @return A User if it finds a match,
+     *         null otherwise.
+     */
+    User findUserByEmailAddress(String emailAddress);
 }
