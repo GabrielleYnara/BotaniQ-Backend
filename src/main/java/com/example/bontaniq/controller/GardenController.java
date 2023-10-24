9af27ba02e63124e30cc885623f56c74c14f48ed
Gabrielle Ynara <gabrielleynara@ymail.com>
2023-10-24 11:29:41 -0500

    Implement GardenController

---
+++ src/main/java/com/example/bontaniq/controller/GardenController.java
@@ -0,0 +1,43 @@
+package com.example.bontaniq.controller;
+
+import com.example.bontaniq.model.Garden;
+import com.example.bontaniq.service.GardenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+import java.util.logging.Logger;
+
+@RestController
+@RequestMapping(path="/gardens") //http://localhost:9092/gardens
+public class GardenController extends SetupController{
+    private final GardenService gardenService;
+    private Logger logger = Logger.getLogger(getClass().getName());
+
+    @Autowired
+    public GardenController(GardenService gardenService) {
+        this.gardenService = gardenService;
+    }
+
+    @PostMapping(path = "/") //http://localhost:9092/gardens/
+    public ResponseEntity<?> createGarden(@RequestBody Garden garden){
+        logger.info("Attempt to create a new Garden.");
+        Optional<Garden> newGarden = gardenService.createGarden(garden);
+        if (newGarden.isPresent()){
+            requestResponse.put("message", "Garden creation completed.");
+            requestResponse.put("data", newGarden);
+            return new ResponseEntity<>(requestResponse, HttpStatus.CREATED);
+        } else {
+//            logger.severe("Garden creation failed!");
+            requestResponse.put("message", "Garden creation failed!");
+            logger.severe(requestResponse.get("message").toString());
+            return new ResponseEntity<>(requestResponse, HttpStatus.CONFLICT);
+        }
+    }
+}
