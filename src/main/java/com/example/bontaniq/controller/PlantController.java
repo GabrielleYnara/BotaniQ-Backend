e5903abc375a115d5d79ce076c17202ccc0dc9cb
Gabrielle Ynara <gabrielleynara@ymail.com>
2023-10-24 18:01:55 -0500

    Implement create plant feature

--- src/main/java/com/example/bontaniq/controller/PlantController.java
+++ src/main/java/com/example/bontaniq/controller/PlantController.java
@@ -1,17 +1,41 @@
 package com.example.bontaniq.controller;
 
+import com.example.bontaniq.model.Garden;
+import com.example.bontaniq.model.Plant;
 import com.example.bontaniq.service.PlantService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Optional;
+
 @Controller
-@RequestMapping(path="/gardens/{gardenId}/plants") // localhost:9092//gardens/{gardenId}/plants
+@RequestMapping(path="/gardens/{gardenId}/plants") // localhost:9092/gardens/{gardenId}/plants
 public class PlantController extends SharedResourceContainer{
     private final PlantService plantService;
 
     @Autowired
     public PlantController(PlantService plantService) {
         this.plantService = plantService;
+    }
+
+    @PostMapping(path = "/") //http://localhost:9092/gardens/{gardenId}/plants/
+    public ResponseEntity<?> createGarden(@RequestBody Plant plant, @PathVariable(value = "gardenId") Long gardenId){
+        logger.info("Attempt to create a new Plant.");
+        Optional<Plant> newPlant = plantService.createPlant(plant, gardenId);
+        if (newPlant.isPresent()){
+            requestResponse.put("message", "Plant saved successfully.");
+            requestResponse.put("data", newPlant);
+            return new ResponseEntity<>(requestResponse, HttpStatus.CREATED);
+        } else {
+            requestResponse.put("message", "Plant creation failed!");
+            logger.severe(requestResponse.get("message").toString());
+            return new ResponseEntity<>(requestResponse, HttpStatus.CONFLICT);
+        }
     }
 }
--- src/main/java/com/example/bontaniq/service/PlantService.java
+++ src/main/java/com/example/bontaniq/service/PlantService.java
@@ -1,15 +1,36 @@
 package com.example.bontaniq.service;
 
+import com.example.bontaniq.exception.exception.InformationExistException;
+import com.example.bontaniq.exception.exception.InformationNotFoundException;
+import com.example.bontaniq.model.Garden;
+import com.example.bontaniq.model.Plant;
 import com.example.bontaniq.repository.PlantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
-public class PlantService {
+public class PlantService extends ServiceSharedResources{
     private final PlantRepository plantRepository;
+    private final GardenService gardenService;
+
 
     @Autowired
-    public PlantService(PlantRepository plantRepository) {
+    public PlantService(PlantRepository plantRepository, GardenService gardenService) {
         this.plantRepository = plantRepository;
+        this.gardenService = gardenService;
+    }
+
+    public Optional<Plant> createPlant(Plant plant, Long gardenId){
+        logger.info("Initializing create plant: ");
+        Optional<Garden> gardenOptional = gardenService.getGardenById(gardenId);
+        if(gardenOptional.isPresent()){
+            plant.setGarden(gardenOptional.get());
+            logger.info("Plant created!");
+            return Optional.of(plantRepository.save(plant));
+        } else {
+            throw new InformationNotFoundException("Garden not found! Can't create a plant without a garden.");
+        }
     }
 }
