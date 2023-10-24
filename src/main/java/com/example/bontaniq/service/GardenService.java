6a2089197a3f9b0addbe1c65860306f4cec7e6e1
Gabrielle Ynara <gabrielleynara@ymail.com>
2023-10-24 11:29:17 -0500

    Implement GardenService

---
+++ src/main/java/com/example/bontaniq/service/GardenService.java
@@ -0,0 +1,41 @@
+package com.example.bontaniq.service;
+
+import com.example.bontaniq.exception.exception.InformationExistException;
+import com.example.bontaniq.model.Garden;
+import com.example.bontaniq.model.User;
+import com.example.bontaniq.repository.GardenRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.logging.Logger;
+
+@Service
+public class GardenService {
+    private final GardenRepository gardenRepository;
+    private final UserService userService;
+
+    private Logger logger = Logger.getLogger(GardenRepository.class.getName());
+
+    @Autowired
+    public GardenService(GardenRepository gardenRepository, UserService userService) {
+        this.gardenRepository = gardenRepository;
+        this.userService = userService;
+    }
+
+    public Optional<Garden> createGarden(Garden garden){
+        logger.info("Initializing garden creation:");
+        User currentUser = userService.getCurrentLoggedInUser();
+        // check if a garden with description already exists
+        Optional<Garden> gardenOptional = gardenRepository.findByDescriptionAndUserId(garden.getDescription(), currentUser.getId());
+        // if it does, returns error
+        if (gardenOptional.isPresent()){
+            throw new InformationExistException("Garden with description '" + garden.getDescription() + "' already exists.");
+        }
+        else {
+            garden.setUser(currentUser);
+            logger.info("Garden " + garden.getDescription() + " created!");
+            return Optional.of(gardenRepository.save(garden));
+        }
+    }
+}
