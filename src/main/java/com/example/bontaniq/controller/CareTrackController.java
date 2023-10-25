daa87596dd8e292dcd5d97f8317fdf9b4de93386
Gabrielle Ynara <gabrielleynara@ymail.com>
2023-10-25 11:52:12 -0500

    Implement Feature: Register Care for a Specific Plant

---
+++ src/main/java/com/example/bontaniq/controller/CareTrackController.java
@@ -0,0 +1,37 @@
+package com.example.bontaniq.controller;
+
+import com.example.bontaniq.model.CareTrack;
+import com.example.bontaniq.service.CareTrackService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path = "/gardens/{gardenId}/plants/{plantId}/cares/{careId}/care-tracker") // localhost:9092/gardens/{gardenId}/plants/{plantId}/cares/{careId}/care-tracker/
+public class CareTrackController extends ControllerSharedResources{
+    private final CareTrackService careTrackService;
+
+    @Autowired
+    public CareTrackController(CareTrackService careTrackService) {
+        this.careTrackService = careTrackService;
+    }
+
+    @PostMapping(path = "/") //http://localhost:9092/gardens/{gardenId}/plants/{plantId}/cares/
+    public ResponseEntity<?> registerCareTrack(@PathVariable(value = "gardenId") Long gardenId, @PathVariable(value = "plantId") Long plantId, @PathVariable(value = "careId") Long careTypeId, @RequestBody CareTrack careTrack){
+        logger.info("Attempt to create a new Care Track.");
+        Optional<CareTrack> newCareTrack = careTrackService.registerCareTrack(gardenId, plantId, careTypeId, careTrack);
+        if (newCareTrack.isPresent()){
+            requestResponse.put("message", "Plant care registered successfully.");
+            requestResponse.put("data", newCareTrack);
+            return new ResponseEntity<>(requestResponse, HttpStatus.CREATED);
+        } else {
+            requestResponse.put("message", "Plant care registration failed!");
+            logger.severe(requestResponse.get("message").toString());
+            return new ResponseEntity<>(requestResponse, HttpStatus.CONFLICT);
+        }
+    }
+
+}
--- src/main/java/com/example/bontaniq/service/CareTrackService.java
+++ src/main/java/com/example/bontaniq/service/CareTrackService.java
@@ -1,19 +1,55 @@
 package com.example.bontaniq.service;
 
+import com.example.bontaniq.exception.exception.IllegalArgumentException;
+import com.example.bontaniq.exception.exception.InformationNotFoundException;
 import com.example.bontaniq.model.CareTrack;
+import com.example.bontaniq.model.CareType;
+import com.example.bontaniq.model.Plant;
 import com.example.bontaniq.repository.CareTrackRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
 public class CareTrackService extends ServiceSharedResources{
     private final CareTrackRepository careTrackRepository;
+    private final PlantService plantService;
 
     @Autowired
-    public CareTrackService(CareTrackRepository careTrackRepository) {
+    public CareTrackService(CareTrackRepository careTrackRepository, PlantService plantService) {
         this.careTrackRepository = careTrackRepository;
+        this.plantService = plantService;
     }
+    public Optional<CareTrack> registerCareTrack(Long gardenId, Long plantId, Long careTypeId, CareTrack careTrack) {
+        logger.info("Initializing register plant care: ");
+        Optional<Plant> plantOptional = plantService.getPlantById(plantId, gardenId);
+        if (plantOptional.isPresent()){
+            Optional<CareType> careTypeOptional = plantOptional.get().getCareTypeList().stream()
+                    .filter((careType -> Objects.equals(careType.getId(), careTypeId) ))
+                    .findFirst();
+            if (careTypeOptional.isEmpty()){
+                String message = "Care type with id " + careTypeId + " not found";
+                logger.severe(message);
+                throw new InformationNotFoundException(message);
+            }
+            careTrack.setCareType(careTypeOptional.get());
+            if (careTrack.getDate() == null){
+                String message = "Care track needs a date to be saved.";
+                logger.severe(message);
+                throw new IllegalArgumentException(message);
+            }
+            logger.info("Care Track saved successfully!");
+            return Optional.of(careTrackRepository.save(careTrack));
+        } else {
+            return Optional.empty();
+        }
+
+    } //ToDo: once the caretype get method is implemented, update this.
+    //search garden
+    //search plant
+    //search care type
+    //save care track
 
 }
