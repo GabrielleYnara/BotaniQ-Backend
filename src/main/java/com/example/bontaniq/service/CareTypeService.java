8285d4dcb0340a621f7ffbd4f34413db33c78ded
Gabrielle Ynara <gabrielleynara@ymail.com>
2023-10-24 22:17:14 -0500

    Implement CareTypeService

---
+++ src/main/java/com/example/bontaniq/service/CareTypeService.java
@@ -0,0 +1,17 @@
+package com.example.bontaniq.service;
+
+import com.example.bontaniq.repository.CareTypeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CareTypeService extends ServiceSharedResources{
+    private CareTypeRepository careTypeRepository;
+    private PlantService plantService;
+
+    @Autowired
+    public CareTypeService(CareTypeRepository careTypeRepository, PlantService plantService) {
+        this.careTypeRepository = careTypeRepository;
+        this.plantService = plantService;
+    }
+}
