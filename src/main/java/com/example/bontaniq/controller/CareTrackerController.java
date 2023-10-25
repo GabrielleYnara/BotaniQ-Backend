23ea42ba07995ded8bcb1c3914e7875a27ad6407
Gabrielle Ynara <gabrielleynara@ymail.com>
2023-10-25 11:08:30 -0500

    rename Care Tracker to Care Track

--- src/main/java/com/example/bontaniq/controller/CareTrackerController.java
+++ src/main/java/com/example/bontaniq/controller/CareTrackerController.java
@@ -1,6 +1,6 @@
 package com.example.bontaniq.controller;
 
-import com.example.bontaniq.service.CareTrackerService;
+import com.example.bontaniq.service.CareTrackService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,11 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(path = "/gardens/{gardenId}/plants/{plantId}/cares/{careId}/care-tracker") // localhost:9092/gardens/{gardenId}/plants/{plantId}/cares/{careId}/care-tracker/
 public class CareTrackerController extends ControllerSharedResources{
-    private final CareTrackerService careTrackerService;
+    private final CareTrackService careTrackService;
 
     @Autowired
-    public CareTrackerController(CareTrackerService careTrackerService) {
-        this.careTrackerService = careTrackerService;
+    public CareTrackerController(CareTrackService careTrackService) {
+        this.careTrackService = careTrackService;
     }
 
 }
