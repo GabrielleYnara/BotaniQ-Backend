d4445cd88386cbbd1b20e7fb6670f2b981c693f6
Gabrielle Ynara <gabrielleynara@ymail.com>
2023-10-26 13:07:52 -0500

    Refactor loginUser response to return the user and jwt

--- src/main/java/com/example/bontaniq/model/response/LoginResponse.java
+++ src/main/java/com/example/bontaniq/model/response/LoginResponse.java
@@ -1,11 +1,18 @@
 package com.example.bontaniq.model.response;
 
+import com.example.bontaniq.model.User;
+
 public class LoginResponse {
 
     public String jwt;
+    public User user;
 
     public LoginResponse(String jwt) {
         this.jwt = jwt;
     }
 
+    public LoginResponse(String jwt, User user) {
+        this.jwt = jwt;
+        this.user = user;
+    }
 }
