db5aa4326640eb4003b7bbce53c5ca7c6f7ea881
Gabrielle Ynara <gabrielleynara@ymail.com>
2023-10-27 08:51:43 -0500

    Implement View Plant care tracker feature

--- src/main/java/com/example/bontaniq/controller/PlantController.java
+++ src/main/java/com/example/bontaniq/controller/PlantController.java
@@ -1,6 +1,7 @@
 package com.example.bontaniq.controller;
 
 import com.example.bontaniq.model.Plant;
+import com.example.bontaniq.service.CareTrackService;
 import com.example.bontaniq.service.PlantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -8,16 +9,19 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @Controller
 @RequestMapping(path="/gardens/{gardenId}/plants") // localhost:9092/gardens/{gardenId}/plants
 public class PlantController extends ControllerSharedResources {
     private final PlantService plantService;
+    private final CareTrackService careTrackService;
 
     @Autowired
-    public PlantController(PlantService plantService) {
+    public PlantController(PlantService plantService, CareTrackService careTrackService) {
         this.plantService = plantService;
+        this.careTrackService = careTrackService;
     }
 
     @PostMapping(path = "/") //http://localhost:9092/gardens/{gardenId}/plants/
@@ -49,4 +53,19 @@ public class PlantController extends ControllerSharedResources {
             return new ResponseEntity<>(requestResponse, HttpStatus.NOT_FOUND);
         }
     }
+    @GetMapping(path = "/{plantId}/care-tracker/") //http://localhost:9092/gardens/{gardenId}/plants/{plantId}/
+    public ResponseEntity<?> getPlantCareTracker(@PathVariable(value = "plantId") Long plantId){
+        logger.info("Attempt to retrieve a plant care tracker with plantId " + plantId);
+        List<Object[]> careTrackerList = careTrackService.getAllTracksBy(plantId);
+        if (!careTrackerList.isEmpty()){
+            requestResponse.put("message", "Plant care tracker retrieved successfully.");
+            requestResponse.put("data", careTrackerList);
+            return new ResponseEntity<>(requestResponse, HttpStatus.OK);
+        } else {
+            requestResponse.put("message", "Plant care tracker still empty.");
+            logger.severe(requestResponse.get("message").toString());
+            return new ResponseEntity<>(requestResponse, HttpStatus.NOT_FOUND);
+        }
+    }
+
 }
