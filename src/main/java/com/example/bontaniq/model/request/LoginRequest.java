a60c2052540595cc8a609a49b30bbaa97fd38607
Gabrielle Ynara <gabrielleynara@ymail.com>
2023-10-31 11:09:02 -0500

    Add relevant Javadoc to LoginRequest

--- src/main/java/com/example/bontaniq/model/request/LoginRequest.java
+++ src/main/java/com/example/bontaniq/model/request/LoginRequest.java
@@ -1,8 +1,19 @@
 package com.example.bontaniq.model.request;
 
+/**
+ * Represents the data structure for handling login HTTP request body.<br>
+ * This model captures the necessary information required for user authentication, the email address and password.
+ */
 public class LoginRequest {
 
+    /**
+     * The email of the user attempting to log in.
+     */
     private String emailAddress;
+
+    /**
+     * The password provided by the user for authentication.
+     */
     private String password;
 
     public String getEmailAddress() {
