fd7f91db0aba8951ac4e34752d360865f330231d
Gabrielle Ynara <gabrielleynara@ymail.com>
2023-10-31 11:38:26 -0500

    Add relevant Javadoc to CareTrackRepository

--- src/main/java/com/example/bontaniq/repository/CareTrackRepository.java
+++ src/main/java/com/example/bontaniq/repository/CareTrackRepository.java
@@ -8,8 +8,21 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+/**
+ * A Spring Data JPA repository for managing CareTrack entities.<br>
+ * It extends JpaRepository, which provides methods for basic CRUD operations.
+ */
 @Repository
 public interface CareTrackRepository extends JpaRepository<CareTrack, Long> { //Entity, id data type
+    /**
+     * Retrieves care track details associated with a plant id.
+     * <p>
+     *     The method returns a list of objects containing the date, done status, and care type for each care track record related to the given plant id.
+     * </p>
+     *
+     * @param plantId The id of the associated plant.
+     * @return A list of objects containing care track details.
+     */
     @Query("SELECT ct.date, ct.done, c.type\n" +
             "FROM CareTrack ct\n" +
             "JOIN CareType c ON ct.careType.id = c.id\n" +
