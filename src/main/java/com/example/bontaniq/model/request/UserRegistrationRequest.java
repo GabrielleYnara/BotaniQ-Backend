e630e4a20a1bdd8bc732dbf212acbee5d90d5ed8
Gabrielle Ynara <gabrielleynara@ymail.com>
2023-10-31 11:16:39 -0500

    Add relevant Javadoc to UserRegistrationRequest

--- src/main/java/com/example/bontaniq/model/request/UserRegistrationRequest.java
+++ src/main/java/com/example/bontaniq/model/request/UserRegistrationRequest.java
@@ -2,9 +2,18 @@ package com.example.bontaniq.model.request;
 
 import com.example.bontaniq.model.Profile;
 import com.example.bontaniq.model.User;
-
+/**
+ * Represents the data structure for handling user registration HTTP request body.<br>
+ * This model captures the necessary information required for user registration, combining both user and the associated profile.
+ */
 public class UserRegistrationRequest {
+    /**
+     * User details, including email and password, required for registration.
+     */
     private User user;
+    /**
+     * Profile details associated with the user, like first name, last name, and bio.
+     */
     private Profile profile;
 
     public User getUser() {
