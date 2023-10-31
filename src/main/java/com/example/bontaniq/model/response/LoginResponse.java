93aaa06951cc68e34a7cd403bec51bc75b2fb125
Gabrielle Ynara <gabrielleynara@ymail.com>
2023-10-31 11:21:09 -0500

    Add relevant Javadoc to LoginResponse

--- src/main/java/com/example/bontaniq/model/response/LoginResponse.java
+++ src/main/java/com/example/bontaniq/model/response/LoginResponse.java
@@ -2,9 +2,21 @@ package com.example.bontaniq.model.response;
 
 import com.example.bontaniq.model.User;
 
+/**
+ * Represents the data structure for the login response sent to the client after successful authentication.<br>
+ * This model captures the JWT token and, optionally, the user details.
+ */
 public class LoginResponse {
 
+    /**
+     * The JSON Web Token generated after successful authentication.<br>
+     * Or error message after failed authentication.
+     */
     public String jwt;
+
+    /**
+     * The details of the authenticated user.
+     */
     public User user;
 
     public LoginResponse(String jwt) {
