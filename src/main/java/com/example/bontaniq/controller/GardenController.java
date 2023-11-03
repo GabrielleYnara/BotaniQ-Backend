c8a6c68782cb0b2e0071b7838390ab80a3d22487
Gabrielle Ynara <gabrielleynara@ymail.com>
2023-11-03 13:08:37 -0500

    Add relevant Javadoc to GardenController

--- src/main/java/com/example/bontaniq/controller/GardenController.java
+++ src/main/java/com/example/bontaniq/controller/GardenController.java
@@ -10,15 +10,29 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Handles HTTP requests related to garden operations.
+ */
 @RestController
 @RequestMapping(path="/gardens") //http://localhost:9092/gardens
 public class GardenController extends ControllerSharedResources {
     private final GardenService gardenService;
 
+    /**
+     * Injects dependencies and enables gardenController to access the resources.
+     * @param gardenService The Service responsible for garden business logic operations.
+     */
     @Autowired
     public GardenController(GardenService gardenService) {
         this.gardenService = gardenService;
     }
+
+    /**
+     * Creates a new garden and associates it currently logged-in user.
+     *
+     * @param garden The garden object containing the details for creation.
+     * @return A ResponseEntity containing the created garden or an error message.
+     */
 
     @PostMapping(path = "/") //http://localhost:9092/gardens/
     public ResponseEntity<?> createGarden(@RequestBody Garden garden){
@@ -35,6 +49,11 @@ public class GardenController extends ControllerSharedResources {
         }
     }
 
+    /**
+     * Retrieves a specific garden by its id.
+     * @param gardenId The garden id to retrieve.
+     * @return A ResponseEntity containing the requested garden or an error message.
+     */
     @GetMapping(path = "/{gardenId}/") //http://localhost:9092/gardens/1/
     public ResponseEntity<?> getGarden(@PathVariable(value = "gardenId") Long gardenId){
         logger.info("Attempt to retrieve a garden with id " + gardenId);
@@ -50,6 +69,10 @@ public class GardenController extends ControllerSharedResources {
         }
     }
 
+    /**
+     * Retrieves all the gardens associated with the currently logged-in user.
+     * @return A ResponseEntity containing the list of garden records or an error message.
+     */
     @GetMapping(path = "/") //http://localhost:9092/gardens/
     public ResponseEntity<?> getAllGardens(){
         logger.info("Attempt to retrieve a garden list.");
@@ -65,6 +88,13 @@ public class GardenController extends ControllerSharedResources {
         }
     }
 
+    /**
+     * Updates the garden, allowing for individual or multiple attribute changes.
+     *
+     * @param garden Garden object with new details.
+     * @param gardenId The garden id targeted.
+     * @return A ResponseEntity containing the updated garden records or an error message.
+     */
     @PutMapping(path = "/{gardenId}/") //http://localhost:9092/gardens/1/
     public ResponseEntity<?> updateGarden(@RequestBody Garden garden, @PathVariable(value = "gardenId") Long gardenId) throws IllegalAccessException {
         logger.info("Attempt to update garden.");
@@ -77,6 +107,5 @@ public class GardenController extends ControllerSharedResources {
             requestResponse.put("message", "Garden update failed!");
             return new ResponseEntity<>(requestResponse, HttpStatus.CONFLICT);
         }
-
     }
 }
