48840f154d84e1201fb2b0a1b48a2bfd8356d093
Gabrielle Ynara <gabrielleynara@ymail.com>
2023-11-03 12:02:04 -0500

    Add relevant Javadoc to PlantService

--- src/main/java/com/example/bontaniq/service/PlantService.java
+++ src/main/java/com/example/bontaniq/service/PlantService.java
@@ -12,11 +12,24 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
+/**
+ * Represents the Plant Service, responsible for housing business logic related to plants.<br>
+ *<p>
+ *     This class serves as an intermediary between the controller and the repository,
+ *     invoking the repository to perform CRUD operations on users.
+ *</p>
+ */
 @Service
 public class PlantService extends ServiceSharedResources{
     private final PlantRepository plantRepository;
     private final GardenService gardenService;
 
+    /**
+     * Injects dependencies and enables plantService to access the resources.
+     *
+     * @param plantRepository The repository for plant-related CRUD operations.
+     * @param gardenService The service for garden-related business logic operations.
+     */
 
     @Autowired
     public PlantService(PlantRepository plantRepository, GardenService gardenService) {
@@ -24,6 +37,15 @@ public class PlantService extends ServiceSharedResources{
         this.gardenService = gardenService;
     }
 
+    /**
+     * Creates a new plant associated with the currently logged-in user and the gardenId provided.
+     *
+     * @param plant The plant details to be created.
+     * @param gardenId The garden id, which this plant belongs to.
+     * @return An Optional with the recently created plant;
+     * @throws IllegalArgumentException If the 'name' or 'type' fields of the plant are null or empty.
+     * @throws InformationNotFoundException If garden not found.
+     */
     public Optional<Plant> createPlant(Plant plant, Long gardenId){
         logger.info("Initializing create plant: ");
         Optional<Garden> gardenOptional = gardenService.getGardenById(gardenId);
@@ -33,13 +55,20 @@ public class PlantService extends ServiceSharedResources{
                 logger.info("Plant created!");
                 return Optional.of(plantRepository.save(plant));
             } else {
-                throw new IllegalArgumentException("All fields must be provided.");
+                throw new IllegalArgumentException("The 'name' and 'type' fields of the plant must not be null or empty.");
             }
         } else {
             throw new InformationNotFoundException("Garden not found! Can't create a plant without a garden.");
         }
     }
 
+    /**
+     * Retrieves a plant associated with currently logged-in user by the given garden, and plant id.
+     * @param plantId The plant id.
+     * @param gardenId The garden id.
+     * @return An Optional with the Plant object if successful retrieval.
+     * @throws InformationNotFoundException If the garden with the given 'gardenId' is not found, or if a plant with the given 'plantId' is not found within the located garden.
+     */
     public Optional<Plant> getPlantById(Long plantId, Long gardenId){
         logger.info("Initializing retrieve plant by id.");
         Optional<Garden> garden = gardenService.getGardenById(gardenId);
@@ -51,7 +80,7 @@ public class PlantService extends ServiceSharedResources{
                 logger.info("Found " + plantFound.get().getName());
                 return plantFound;
             } else {
-                String message = "Plant not found.";
+                String message = "Plant not found in Garden :" + garden.get().getDescription();
                 logger.severe(message);
                 throw new InformationNotFoundException(message);
             }
