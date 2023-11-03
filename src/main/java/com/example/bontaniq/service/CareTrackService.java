ff2f2bba2134af6da4448312bf72e09182005c6e
Gabrielle Ynara <gabrielleynara@ymail.com>
2023-11-03 12:34:35 -0500

    Add relevant Javadoc to CareTrackService

--- src/main/java/com/example/bontaniq/service/CareTrackService.java
+++ src/main/java/com/example/bontaniq/service/CareTrackService.java
@@ -11,16 +11,41 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
+/**
+ * Represents the Care Track Service, responsible for housing business logic related to care tracks.<br>
+ *<p>
+ *     This class serves as an intermediary between the controller and the repository,
+ *     invoking the repository to perform CRUD operations on care tracks.
+ *</p>
+ */
 @Service
 public class CareTrackService extends ServiceSharedResources{
     private final CareTrackRepository careTrackRepository;
     private final PlantService plantService;
 
+    /**
+     * Injects dependencies and enables careTypeService to access the resources.
+     *
+     * @param careTrackRepository The repository for careTrack-related CRUD operations.
+     * @param plantService The service plant business logic operations.
+     */
     @Autowired
     public CareTrackService(CareTrackRepository careTrackRepository, PlantService plantService) {
         this.careTrackRepository = careTrackRepository;
         this.plantService = plantService;
     }
+
+    /**
+     * Registers a new care track for a specific care type associated with a plant and garden.
+     *
+     * @param gardenId The garden id where the plant is located.
+     * @param plantId The plant id for which the care is being tracked.
+     * @param careTypeId The care type id that the care track is recording.
+     * @param careTrack The CareTrack object with details to be saved.
+     * @return An Optional with the saved CareTrack object if successful; otherwise, an empty Optional.
+     * @throws InformationNotFoundException If the plant or care type is not found.
+     * @throws IllegalArgumentException If the care track date is null.
+     */
     public Optional<CareTrack> registerCareTrack(Long gardenId, Long plantId, Long careTypeId, CareTrack careTrack) {
         logger.info("Initializing register plant care: ");
         Optional<Plant> plantOptional = plantService.getPlantById(plantId, gardenId);
@@ -47,6 +72,13 @@ public class CareTrackService extends ServiceSharedResources{
 
     }
 
+    /**
+     * Retrieves all care track records for a given plant.
+     *
+     * @param plantId The plant id for which to retrieve care tracks.
+     * @return A list of care track records for the specified plant.
+     * @throws InformationNotFoundException If no care track records are found for the plant.
+     */
     public List<Object[]> getAllTracksBy(Long plantId){
         logger.info("Initializing retrieve all gardens:");
         List<Object[]> plantCareTracker = careTrackRepository.findCareTrackDetailsByPlantId(plantId);
