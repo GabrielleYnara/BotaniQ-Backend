6205ecd00521deb8ff60e7304938e38b2bc42ac5
Gabrielle Ynara <gabrielleynara@ymail.com>
2023-11-03 13:21:14 -0500

    Add relevant Javadoc to CareTrackController

--- src/main/java/com/example/bontaniq/controller/CareTrackController.java
+++ src/main/java/com/example/bontaniq/controller/CareTrackController.java
@@ -8,17 +8,33 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
-
+/**
+ * Handles HTTP requests for tracking the care activities for plants within a garden.
+ */
 @RestController
 @RequestMapping(path = "/gardens/{gardenId}/plants/{plantId}/cares/{careId}/care-tracker") // localhost:9092/gardens/{gardenId}/plants/{plantId}/cares/{careId}/care-tracker/
 public class CareTrackController extends ControllerSharedResources{
     private final CareTrackService careTrackService;
 
+    /**
+     * Injects dependencies and enables careTrackController to access the resources.
+     *
+     * @param careTrackService The service for care track operations.
+     */
     @Autowired
     public CareTrackController(CareTrackService careTrackService) {
         this.careTrackService = careTrackService;
     }
 
+    /**
+     * Registers a new care activity for a specific care type associated with a plant in a garden.
+     *
+     * @param gardenId  The garden id where the plant resides.
+     * @param plantId   The plant id for which care is being tracked.
+     * @param careTypeId The care type id associated with this care track.
+     * @param careTrack The CareTrack object containing care details.
+     * @return A ResponseEntity containing the registered care track or an error message.
+     */
     @PostMapping(path = "/") //http://localhost:9092/gardens/{gardenId}/plants/{plantId}/cares/{careId}/care-tracker/
     public ResponseEntity<?> registerCareTrack(@PathVariable(value = "gardenId") Long gardenId, @PathVariable(value = "plantId") Long plantId, @PathVariable(value = "careId") Long careTypeId, @RequestBody CareTrack careTrack){
         logger.info("Attempt to create a new Care Track.");
